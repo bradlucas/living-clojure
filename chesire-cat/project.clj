cc1fe2092bcaa07cefc35af48215a63e8fd5ca55
Brad Lucas <brad@beaconhill.com>
2016-10-15 22:34:18 -0400

    updating all library versions

--- chesire-cat/project.clj
+++ chesire-cat/project.clj
@@ -1,20 +1,20 @@
-(defproject chesire-cat "0.1.0-SNAPSHOT"
+(defproject chesire-cat "1.0.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]
-                 [cheshire "5.5.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.1"]
+                 [ring/ring-defaults "0.2.1"]
+                 [cheshire "5.6.3"]
                  [ring/ring-json "0.4.0"]
-                 [org.clojure/clojurescript "1.7.48"]
+                 [org.clojure/clojurescript "1.9.229"]
                  ]
-  :plugins [[lein-ring "0.8.13"]
-            [lein-cljsbuild "1.1.0"]]
+  :plugins [[lein-ring "0.9.7"]
+            [lein-cljsbuild "1.1.4"]]
   :ring {:handler chesire-cat.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}}
+                        [ring-mock "0.3.0"]]}}
   :cljsbuild {
               :builds [{
                         :source-paths ["src-cljs"]
