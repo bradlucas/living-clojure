2477f49de1050622300bb31d6845e59bf802b92b
Brad Lucas <brad@beaconhill.com>
2016-10-15 22:34:37 -0400

    version 1.8.0 of clojure

--- livingclojure/project.clj
+++ livingclojure/project.clj
@@ -1,6 +1,6 @@
-(defproject livingclojure "0.1.0-SNAPSHOT"
+(defproject livingclojure "1.0.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]])
